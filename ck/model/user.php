e05807310d77c982633ba7912d0cd1bcfc292857
HongHuy2k4 <dhuy24309@gmail.com>
2024-01-15 17:38:20 +0700

    abc

--- ck/model/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-function checkuser ($user, $pass){
-$conn-connectdb();
-$stmt = $conn->prepare("SELECT * FROM tbl_user WHERE _user='".$user."' AND $stmt->execute();
-$result = $stmt->setFetchMode (PDO::FETCH_ASSOC);
-$kq=$stmt->fetchAll();
-return $kq[0]['role'];
-}
-?>
--- ck/view/login.php
+++
@@ -1,8 +0,0 @@
-
-
-<form action="login.php" method="post">
-<label> Email </label>
-<input type="text" name="email">
-<label> Password</label>
-<input type="password" name="password">
-<button type="submit" name="dangnhap" >Đăng nhập </button> </form>
